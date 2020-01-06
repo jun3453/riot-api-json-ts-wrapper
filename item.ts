ecfb66c08707b05561e4b37c7ec0c06d0e391266
junki <jun34530811@gmail.com>
2020-01-06 15:39:06 +0900

    item.json wrapper

---
+++ item.ts
@@ -0,0 +1,41 @@
+import { image } from "./global";
+
+export type items = {
+    type: string;
+    format: string;
+    version: string;
+    data: {[name: string]: item}[];
+    groups: groups[]; 
+    tree: tree[];
+}
+
+export type item = {
+    name: string;
+    description: string;
+    colloq: string;
+    plaintext: string;
+    into: string[];
+    image: image;
+    gold: gold;
+    tags: string[];
+    maps: {key: string, value: boolean}; 
+    stats: {key: string, value: boolean};
+    depth?: number;
+}
+
+type gold = {
+    base: number;
+    purchasable: boolean;
+    total: number;
+    sell: number;
+}
+
+type groups = {
+    id: string,
+    MaxGroupOwnable: string,
+}
+
+type tree = {
+    header: string,
+    tags: string[],
+}
